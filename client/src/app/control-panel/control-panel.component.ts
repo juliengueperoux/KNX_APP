1f24501b1b4f7685c5d3817fc03e1e15fe00a553
Yztoc <stephant.thomas.ae@gmail.com>
2019-03-27 08:18:08 +0100

    new feature, opti

--- client/src/app/control-panel/control-panel.component.ts
+++ client/src/app/control-panel/control-panel.component.ts
@@ -10,10 +10,15 @@ import {MatSnackBar} from '@angular/material';
 export class ControlPanelComponent implements OnInit {
 
   numeros =[1,2,3,4]
-
+  interval = 1000;
+  active = true; 
   constructor(private snackBar: MatSnackBar) { }
 
   ngOnInit() {
+  }
+
+  setCSSclass(name,classCss,action) : void{
+    (action) ? document.getElementById(name).classList.add(classCss) : document.getElementById(name).classList.remove(classCss);
   }
 
   openSnackBar(message: string, action: string) {
@@ -31,7 +36,8 @@ export class ControlPanelComponent implements OnInit {
         let nameLight = "svg-light-" + element
         //let nameToggle = "toggle-" + element
         //document.getElementById(nameToggle). = true;
-        document.getElementById(nameLight).classList.add('is-activated');
+        this.setCSSclass(nameLight,'is-activated',true);
+        document.getElementById(nameLight).classList.add();
       })
     }
     else{
@@ -40,7 +46,7 @@ export class ControlPanelComponent implements OnInit {
       });
       this.numeros.forEach(element => {
         let name = "svg-light-" + element
-        document.getElementById(name).classList.remove  ('is-activated');
+        this.setCSSclass(name,'is-activated',false);
       })
     } 
   }
@@ -51,14 +57,14 @@ export class ControlPanelComponent implements OnInit {
         (res.data.success) ? this.openSnackBar("Lampe numéro " + numero + " allumée","Ok") : this.openSnackBar("Error" + res.data,"Ok");
       });
       let name = "svg-light-" + numero;
-      document.getElementById(name).classList.add('is-activated');
+      this.setCSSclass(name,'is-activated',true);
     } 
     else{
       KnxService.stopLight(numero).then((res) =>{
         (res.data.success) ? this.openSnackBar("Lampe numéro " + numero + " éteinte","Ok") : this.openSnackBar("Error" + res.data,"Ok");
       });;
       let name = "svg-light-" + numero;
-      document.getElementById(name).classList.remove('is-activated');
+      this.setCSSclass(name,'is-activated',false);
     } 
   }
 
@@ -67,21 +73,12 @@ export class ControlPanelComponent implements OnInit {
       KnxService.startChase().then((res) =>{
         (res.data.success) ? this.openSnackBar("Chenillard allumé","Ok") : this.openSnackBar("Error" + res.data,"Ok");
       });
-      /*this.numeros.forEach(element => {
-        let nameLight = "svg-light-" + element
-        //let nameToggle = "toggle-" + element
-        //document.getElementById(nameToggle). = true;
-        document.getElementById(nameLight).classList.add('is-activated');
-      })*/
     }
     else{
       KnxService.stopChase().then((res) =>{
         (res.data.success) ? this.openSnackBar("Chenillard éteint","Ok") : this.openSnackBar("Error" + res.data,"Ok");
       });
-      /*this.numeros.forEach(element => {
-        let name = "svg-light-" + element
-        document.getElementById(name).classList.remove  ('is-activated');
-      })*/
+      this.active =false;
     } 
   }
 
