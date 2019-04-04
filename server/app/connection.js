da57131f2e471fd442a036deac0840b8180e5740
AirJulien <julien.gueperoux@gmail.com>
2019-04-04 10:05:38 +0200

    changements mineurs sur l'exports de modules

--- server/app/connection.js
+++ server/app/connection.js
@@ -1,53 +1,63 @@
-var knx = require('knx');
-var variable = require('./variables'); 
+const knx = require('knx');
+const variable = require('./variables');
+let ipAddress = '192.168.0.5'
+let ipPort = 3671
+const connection = new knx.Connection({
+  // ip address and port of the KNX router or interface
+  ipAddr: ipAddress,
+  ipPort: ipPort,
+  // in case you need to specify the multicast interface (say if you have more than one)
+  // define your event handlers here:
+  handlers: {
+    // wait for connection establishment before sending anything!
+    connected: function () {
+      console.log('Connecté');
+    },
 
-var connection = new knx.Connection(
-    {
-    // ip address and port of the KNX router or interface
-    ipAddr: '192.168.0.5', ipPort: 3671,
-    // in case you need to specify the multicast interface (say if you have more than one)
-    // define your event handlers here:
-    handlers: {  
-      // wait for connection establishment before sending anything!
-      connected: function() {
-        console.log('Connecté');
-        },
-    
-      // get notified for all KNX events:
-      event: function(evt, src, dest, value) { 
-      
-        if(dest == "0/3/4"){
-            console.log("Appui dernier à droite : " + variable.main.interval);
-            variable.main.interval +=1000;
+    // get notified for all KNX events:
+    event: function (evt, src, dest, value) {
+
+      if (dest == "0/3/4") {
+        console.log("Appui dernier à droite : " + variable.main.interval);
+        variable.main.interval += 1000;
+      } else if (dest == "0/3/3" && variable.main.interval > 1000) {
+        console.log("Appui dernier à droite : " + variable.main.interval);
+        variable.main.interval -= 1000;
+      } else if (dest == "0/3/2") {
+        if (startChain) {
+          startChain = false;
+        } else {
+          startChain = true;
+          //launch();
         }
-        else if(dest == "0/3/3" && variable.main.interval >1000){
-          console.log("Appui dernier à droite : " + variable.main.interval);
-          variable.main.interval -=1000;
-        }else if(dest == "0/3/2"){
-          if(startChain){
-            startChain = false;
-          }else{
-            startChain = true;
-            //launch();
-          }
-        }else if(dest == "0/3/1"){
-          if(variable.main.sensDirect) variable.main.sensDirect = false;
-          else variable.main.sensDirect = true;
-        }
-  
-      },
-      // get notified on connection errors
-      error: function(connstatus) {
-        console.log("** ERROR: %j", connstatus);
+      } else if (dest == "0/3/1") {
+        if (variable.main.sensDirect) variable.main.sensDirect = false;
+        else variable.main.sensDirect = true;
       }
-    
-    }
-    });
 
-    module.exports = connection;
+    },
+    // get notified on connection errors
+    error: function (connstatus) {
+      console.log("** ERROR: %j", connstatus);
+    }
+  }
+});
+
+setIpPort = function (newIpPort) {
+  ipPort = newIpPort
+};
+
+setIpAdress = function (newIpAddress) {
+  ipAddress = newIpAddress
+};
+
+module.exports = {
+  connection,
+  setIpAdress,
+  setIpPort
+};
 
 // dernier droit 3/4
 // a sa gauche 3/3
 // a sa gauche 3/2
 // dernier gauche 3/1
-      
